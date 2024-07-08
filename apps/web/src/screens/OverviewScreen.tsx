75a6c12ecfdb3406dd999311063a2e11a93b6690
maaaz5 <mohamed.amine12345@gmail.com>
2024-07-08 19:08:54 +0100

    adding some charts (not dynamique yet)

--- apps/web/src/screens/OverviewScreen.tsx
+++ apps/web/src/screens/OverviewScreen.tsx
@@ -1,5 +1,8 @@
 import { TypographyH3 } from "@/Typography/TypographyH3";
 import { TypographySmall } from "@/Typography/TypographySmall";
+import { BarChartsConsultation } from "@/components/charts/BarChartConsultation";
+import { BarChartsComponents } from "@/components/charts/BarCharts";
+import { PieGenderCharts } from "@/components/charts/PieGenderChart";
 import DisplayCard from "@/components/ui/displayCard";
 import { getUser } from "@/features/user/userSlice";
 import { Briefcase, Calendar, User, Video } from "lucide-react";
@@ -14,7 +17,7 @@ const OverviewScreen = () => {
         <TypographySmall>Have a nice day at work</TypographySmall>
       </div>
 
-      <div className="flex gap-10 w-full justify-between flex-wrap">
+      <div className="flex gap-10 w-full flex-wrap">
         <DisplayCard
           Icon={<Calendar />}
           styling="bg-[#7A6EFE]"
@@ -39,13 +42,19 @@ const OverviewScreen = () => {
           key={"ho"}
         />
 
-        <DisplayCard
+        {/* <DisplayCard
           Icon={<Video />}
           styling="bg-[#24A8FA]"
           title="Video Consulting"
           count="28.0k"
           key={"ho"}
-        />
+        /> */}
+      </div>
+      <div className="py-20 grid grid-cols-1 lg:grid-cols-2  xl:grid-cols-3 gap-20">
+        <BarChartsComponents />
+        <PieGenderCharts />
+
+        <BarChartsConsultation />
       </div>
     </div>
   );
